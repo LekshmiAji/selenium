b60c365c84f5b83a1d5be45e3a2e4c24d538d0cb
User <User@DESKTOP-1N09MB5>
2024-11-11 11:57:40 +0530

    committed first assigment

---
+++ Selenium/src/main/java/testscript/InputMessage.java
@@ -0,0 +1,33 @@
+package testscript;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class InputMessage extends Base {
+
+	public static void main(String[] args) {
+
+		InputMessage inputshow=new InputMessage();
+		inputshow.InitialiseBrowser();
+		inputshow.input_Message();
+
+	}
+	public void input_Message()
+	{
+		WebElement input=driver.findElement(By.xpath("//a[@href='simple-form-demo.php']"));
+		input.click();
+		WebElement message=driver.findElement(By.xpath("//input[@id='single-input-field']"));
+		message.sendKeys("Welcome");
+		WebElement click_Showmessage=driver.findElement(By.xpath("//button[@id='button-one']"));
+		click_Showmessage.click();
+		WebElement value1=driver.findElement(By.xpath("//input[@id='value-a']"));
+		value1.sendKeys("23");
+		WebElement value2=driver.findElement(By.xpath("//input[@id='value-b']"));
+		value2.sendKeys("45");
+		WebElement get_total=driver.findElement(By.xpath("//button[@id='button-two']"));
+		get_total.click();
+		
+		
+	}
+
+}
